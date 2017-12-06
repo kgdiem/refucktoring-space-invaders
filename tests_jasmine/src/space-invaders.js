240b7b317ae7863d6d84641713bae5d28c0027e9
Kevin <kevin@Kevins-MBP.localdomain>
2017-12-05 19:23:54 -0500

    resized player

--- tests_jasmine/src/space-invaders.js
+++ tests_jasmine/src/space-invaders.js
@@ -101,7 +101,7 @@ var PubGame = (function() {
 
   var Player = function(game) {
     this.game = game;
-    this.size = { x: 15, y: 15 };
+    this.size = { x: 300, y: 300 };
     this.center = { x: this.game.size.x / 2, y: this.game.size.y - 35 };
     this.keyboarder = new Keyboarder();
   };
